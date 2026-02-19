3f400f9a7610a4a1e0b4df2155a790f06467523e
Brian Rotama Putra <brianrotamaputra@192.168.1.3>
2026-02-19 10:23:53 +0700

    change syntax js on inputspage

--- pages/InputsPage.js
+++ pages/InputsPage.js
@@ -1,11 +1,8 @@
-import { Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage.js';
 import { URLS } from './urls.js';
 
 export class InputsPage extends BasePage {
-  readonly numberInput: Locator;
-
-  constructor(page: Page) {
+  constructor(page) {
     super(page);
     this.numberInput = page.locator('input[type="number"]');
   }
@@ -14,7 +11,7 @@ export class InputsPage extends BasePage {
     await this.page.goto(URLS.INPUTS);
   }
 
-  async fillNumber(value: number) {
-    await this.fill(this.numberInput, value.toString());
+  async fillNumber(value) {
+    await this.fill(this.numberInput, String(value));
   }
 }
